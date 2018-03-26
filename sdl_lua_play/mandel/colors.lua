ad98ee2cdfa99c6a9f8cd0cf78f2f55d2b3498dc
Richard Todd <rwtodd@users.noreply.github.com>
2018-03-25 23:47:01 -0500

    nice color scheme

--- sdl_lua_play/mandel/colors.lua
+++ sdl_lua_play/mandel/colors.lua
@@ -1,10 +1,20 @@
-iterations = 40
-delay = 1
+iterations = 100
+delay = 50 
+
+local palette = {}
+local r, g, b = 0, 256//3, 512//3 
+local dr, dg, db = 1, 1, 1
+for i = 0,512 do
+   palette[i] = { r, g, b }
+   r = r + dr
+   g = g + dg
+   b = b + db
+   if r == 255 or r == 0 then dr = dr * -1 end
+   if g == 255 or g == 0 then dg = dg * -1 end
+   if b == 255 or b == 0 then db = db * -1 end
+end
 
 function color(i)
   if i >= 512 then return 0, 0, 0 end
-
-  if i < 256 then return 0,0,i end
-
-  return (i - 256), (i - 256)//2 , (512-i)//3
+  return table.unpack(palette[i])
 end
