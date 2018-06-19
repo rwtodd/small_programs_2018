0ac98c8f0d28a93a8ec89bc828e3f8a3719bf8e4
Richard Todd <rwtodd@users.noreply.github.com>
2018-06-19 12:05:38 -0500

    made rust version take board size on the command line

--- bishop_puzzle/rust_version/src/main.rs
+++ bishop_puzzle/rust_version/src/main.rs
@@ -1,38 +1,49 @@
 use std::collections::HashSet;
 use std::cmp::{min,max};
 use std::rc::Rc;
+use std::env::args;
 
-const ROWS : usize = 4;
-const COLS : usize = 5;
+type Board = Box<[u8]>;
 
-type Board = Box<[u8; ROWS*COLS]>;
-
-fn empty_board() -> Board {
-   Box::new([0; ROWS*COLS])
+#[derive(Copy,Clone)]
+struct Dimensions { 
+  rows: usize, 
+  cols: usize, 
 }
 
-/// Create a starting board from scratch
-fn starting_board() -> Board {
-   let mut b = empty_board();
-   let mut idx = 0;
-   let max = b.len();
-   while idx < max {
-      b[idx] = 1;
-      b[idx+COLS-1] = 2;
-      idx += COLS;
-   } 
-   b
+fn bishop_for_each<F>(dim : &Dimensions, center: usize, mut action: F) 
+  where F: FnMut(usize)  {
+ 
+  let icols = dim.cols as isize;
+  let cx = (center % dim.cols) as isize;
+  let cy = (center / dim.cols) as isize;
+  let last_row = (dim.rows - 1) as isize;
+  let last_col = (dim.cols - 1) as isize;
+  let xc = last_col - cx;
+  let yc = last_row - cy;
+  let mut idx = (max(cx-cy,0) + icols*max(cy-cx,0)) as usize;
+  let end1 = (min(cx+yc,last_col) + icols*min(cy+xc,last_row)) as usize;
+  while idx <= end1 {
+     if idx != center { action(idx); }  
+     idx += dim.cols + 1;
+  }
+  idx = (min(cx+cy,last_col) + icols*max(cy-xc,0)) as usize;
+  let end2 = (max(cx-yc,0) + icols*min(cx+cy,last_row)) as usize;
+  while idx <= end2 {
+     if idx != center { action(idx); }  
+     idx += dim.cols - 1;
+  }
 }
 
-fn invert_board(b : &mut Board) {
-   for p in b.iter_mut() { if *p > 0 { *p = 3 - *p } };
-}
-
-/// Create a winning board by inverting a starting board
-fn winning_board() -> Board {
-   let mut answer = starting_board();
-   invert_board(&mut answer);
-   answer
+fn attacked_spaces(dim: &Dimensions, b: &Board) -> Board {
+  let mut answer: Board = b.clone();
+  for i in 0..answer.len() {
+     let p = b[i];
+     if p > 0 {
+        bishop_for_each(dim, i,|idx| answer[idx] |= p);
+     }
+  }
+  answer
 }
 
 /// Generates a hash for a given board
@@ -40,19 +51,20 @@ fn hash_board(b: &Board) -> u64 {
    b.iter().fold(0, |acc, v| (acc << 2) | (*v as u64))
 }
 
+
 /// Determine the write value to add to i, to take you
 /// diagonally to j
-fn bishop_delta(i: usize, j: usize) -> isize {
+fn bishop_delta(dim: &Dimensions, i: usize, j: usize) -> isize {
    let diff = j as isize - i as isize;
-   let icols = COLS as isize;
+   let icols = dim.cols as isize;
    diff.signum() *  (icols + if diff % (icols+1) == 0 { 1 } else { -1 }) 
 }
 
 /// Display a board in ASCII
-fn print_board(b : &Board) {
-   for i in 0..ROWS {
-      for j in 0..COLS {
-         print!("{}", match b[i*COLS + j] {
+fn print_board(dim: &Dimensions, b : &Board) {
+   for i in 0..dim.rows {
+      for j in 0..dim.cols {
+         print!("{}", match b[i*dim.cols + j] {
                          0 => "-",
                          1 => "W",
                          2 => "B",
@@ -64,146 +76,133 @@ fn print_board(b : &Board) {
    println!();
 }
 
-fn bishop_for_each<F>(center: usize, mut action: F) 
-  where F: FnMut(usize)  {
- 
-  let icols = COLS as isize;
-  let cx = (center % COLS) as isize;
-  let cy = (center / COLS) as isize;
-  let last_row = (ROWS - 1) as isize;
-  let last_col = (COLS - 1) as isize;
-  let xc = last_col - cx;
-  let yc = last_row - cy;
-  let mut idx = (max(cx-cy,0) + icols*max(cy-cx,0)) as usize;
-  let end1 = (min(cx+yc,last_col) + icols*min(cy+xc,last_row)) as usize;
-  while idx <= end1 {
-     if idx != center { action(idx); }  
-     idx += COLS + 1;
-  }
-  idx = (min(cx+cy,last_col) + icols*max(cy-xc,0)) as usize;
-  let end2 = (max(cx-yc,0) + icols*min(cx+cy,last_row)) as usize;
-  while idx <= end2 {
-     if idx != center { action(idx); }  
-     idx += COLS - 1;
-  }
-}
-
-fn attacked_spaces(b: &Board) -> Board {
-  let mut answer: Board = b.clone();
-  for i in 0..answer.len() {
-     let p = b[i];
-     if p > 0 {
-        bishop_for_each(i,|idx| answer[idx] |= p);
-     }
-  }
-  answer
-}
 
 struct Move {
    board : Board,
    parent : Option<Rc<Move>>,
-   won : bool,
-   b_move : (usize, usize),
+   hash : u64,
+   b_move : (usize, usize)
 }
 
 impl Move {
-  fn new(b: Board, p: Option<Rc<Move>>, w: bool, mv: (usize,usize) ) -> Move {
+  fn new(b: Board, p: Option<Rc<Move>>, mv: (usize,usize) ) -> Move {
+    let bhash = hash_board(&b);
     Move {
       board: b,
       parent: p,
-      won: w,
+      hash: bhash, 
       b_move: mv,
     }
   }
+
 }
 
-fn print_move_list(mv: &Rc<Move>) {
+
+fn print_move_list(dim: &Dimensions, mv: &Rc<Move>) {
   let square = |i| {
     let letters = ['A','B','C','D','E','F','G','H','I'];
-    let row = i % COLS;
-    let col = i / COLS + 1;
+    let row = i % dim.cols;
+    let col = i / dim.cols + 1;
     format!("{}{}", letters[row], col)
   };
-  print_board(&mv.board);
+  print_board(dim, &mv.board);
   if mv.parent.is_some() {
      println!("{} to {}",square(mv.b_move.0),square(mv.b_move.1)); 
      println!();
-     print_move_list(mv.parent.as_ref().unwrap());
+     print_move_list(dim, mv.parent.as_ref().unwrap());
   }
 }
 
 struct Game {
    seen : HashSet<u64>,
-   winning_hash : u64,
+   dimensions : Dimensions,
 }
 
 impl Game {
-  fn new() -> Game {
+  fn new(rows: usize, cols: usize) -> Game {
      Game {  
         seen: HashSet::new(),
-        winning_hash: hash_board(&winning_board())
+        dimensions: Dimensions { rows: rows, cols: cols },
      }
   }
 
-  /// put a board in the seen hash, and return if it's new, and
-  /// if it is a winning board  
-  fn inspect_board(self: &mut Self, b: &Board) -> (bool, bool) {
-     let bh = hash_board(b);
-     (self.seen.insert(bh), bh == self.winning_hash)
+  /// Create a starting move
+  fn starting_move(self: &mut Self) -> Move {
+   let mut b = vec![0u8;self.dimensions.rows*self.dimensions.cols]
+                    .into_boxed_slice(); 
+   let mut idx = 0;
+   let max = b.len();
+   while idx < max {
+      b[idx] = 1;
+      b[idx+self.dimensions.cols-1] = 2;
+      idx += self.dimensions.cols;
+   } 
+   let m = Move::new(b, None, (0,0));
+   self.seen.insert(m.hash);
+   m 
   }
+
+  fn winning_hash(self: &mut Self) -> u64 {
+     let mut sm = self.starting_move();
+     for p in sm.board.iter_mut() { if *p > 0 { *p = 3 - *p } };
+     hash_board(&sm.board)
+  }
+
 
   /// generate all possible next moves, inspect them, and push
   /// them onto the res vector
   fn push_next_moves(self: &mut Self, m: &Rc<Move>, res: &mut Vec<Rc<Move>>) {
      let b = &m.board;
-     let attacked = attacked_spaces(b);
-     let clear_path = |p,i,j| {
-        if ((3 - p) & attacked[j]) != 0 { return false; }
-        let delta = bishop_delta(j,i);
+     let dim = self.dimensions;
+     let attacked = attacked_spaces(&dim, b);
+     let mut try_move = |p,i,j| {
+        if ((3 - p) & attacked[j]) != 0 { return; }
+        let delta = bishop_delta(&dim,j,i);
         let mut x = j;
         while x != i {
-           if b[x] != 0 { return false; }
+           if b[x] != 0 { return ; }
            x = ((x as isize) + delta) as usize;
         } 
-        return true;
+        let mut nb = b.clone(); 
+        nb[j] = p;
+        nb[i] = 0;
+        let nm = Move::new(nb, Some(m.clone()), (i,j));
+        if self.seen.insert(nm.hash) {
+            res.push(Rc::new(nm))
+        } 
      };
      for i in 0..b.len() {
         let p = b[i];
         if p > 0 {
-           bishop_for_each(i, |j| {
-              if clear_path(p,i,j) {
-                 let mut nb: Board = b.clone();
-                 nb[j] = p;
-                 nb[i] = 0;
-                 let (newb, won) = self.inspect_board(&nb);
-                 if newb {
-                     res.push(Rc::new(Move::new(nb, Some(m.clone()), won, (i,j))));
-                 }
-              }
-           });
+           bishop_for_each(&dim, i, |j| try_move(p,i,j));
         }
      }
   }
 }
 
 fn main() {
-   let mut backlog = vec![Rc::new(Move::new(starting_board(), None, false, (0,0)))];
+   let mut args = args();
+   let rows = args.nth(1).unwrap_or_else(|| String::from("4"))
+                  .parse::<usize>().unwrap_or(4);
+   let cols = args.next().unwrap_or_else(|| String::from("5"))
+                  .parse::<usize>().unwrap_or(5);
+   println!("Solving {} by {}...", rows, cols);
+   let mut game = Game::new(rows, cols);
+   let mut backlog = vec![Rc::new(game.starting_move())];
    let mut moves = 0;
-   let mut game = Game::new();
+   let winning_hash = game.winning_hash();
    while backlog.len() > 0 {
       let mut next_moves = Vec::new();
       for mv in backlog.iter() {
+         if mv.hash == winning_hash {
+            print_move_list(&game.dimensions, &mv);
+            return;
+         }
          game.push_next_moves(&mv,&mut next_moves);
       }
       backlog = next_moves; 
       moves += 1;
       println!("Move {}: Moves to consider: {}", moves, backlog.len());
-      for mv in backlog.iter() {
-        if mv.won {
-           print_move_list(&mv);
-           return; 
-        }
-      }
    }   
 }
 
